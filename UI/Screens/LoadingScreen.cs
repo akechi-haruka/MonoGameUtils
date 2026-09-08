db169c07f952cd927910571854ae4ffe01595440
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2026-09-08 14:17:12 +0200

    Remaining features in storage information

--- UI/Screens/LoadingScreen.cs
+++ UI/Screens/LoadingScreen.cs
@@ -4,6 +4,47 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Haruka.MonoGameUtils.UI.Screens;
 
+public class LoadingScreen<T> : Screen {
+
+    private readonly string text;
+    private readonly Func<T> loadFunc;
+    private readonly Action<T> doneFunc;
+    private bool done;
+    private T result;
+        
+    public LoadingScreen(Func<T> loadFunc, Action<T> doneFunc, string text = "NOW LOADING") {
+        this.text = text;
+        this.loadFunc = loadFunc;
+        this.doneFunc = doneFunc;
+        IgnoreOnScreenStack = true;
+    }
+
+    protected override void OnScreenOpened() {
+        new Thread(RunDelegate) {
+            Name = "Loading Delegate"
+        }.Start();
+    }
+
+    public override void OnCreateScreenElements() {
+        AddElement(new ElementText(text, Game.Width / 2, Game.Height / 2, CenterFlags.CenterX | CenterFlags.CenterY));
+    }
+
+    private void RunDelegate() {
+        result = loadFunc.Invoke();
+        done = true;
+    }
+
+    protected override void DrawScreen(GameTime gameTime, SpriteBatch spriteBatch) {
+
+    }
+
+    protected override void UpdateScreen(GameTime gameTime) {
+        if (done) {
+            doneFunc?.Invoke(result);
+        }
+    }
+}
+
 public class LoadingScreen : Screen {
 
     private readonly string text;
